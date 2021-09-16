b1302be18c12d97d39eef369e8e5b7fbaf798ef4
Shashwat <harshshashwat18@gmail.com>
2021-09-16 23:08:25 +0530

    updating reset btn

--- popup.js
+++ popup.js
@@ -1,20 +1,30 @@
-let hr1 = document.querySelector('span.hr1');
-let hr2 = document.querySelector('span.hr2');
-let msec = ((hr1*10)+hr2)*60*60;
-let min1 = document.querySelector('span.min1');
-let min2 = document.querySelector('span.min2');
-msec = msec+ ((min1*10)+min2)*60;
-var sec1 = document.getElementsByClassName('sec1').textContent = '7';
-let sec2 = document.querySelector('span.sec2');
-let is1 = document.getElementById('is1').textContent = "9";
-console.log(sec1);
-msec = msec + ((sec1*10)+sec2);
-console.log();
-let sec = 
+let hr1 = document.querySelector('.hr1');
+let hr2 = document.querySelector('.hr2');
+
+let msec = ((hr1 * 10) + hr2) * 60 * 60;
+
+let min1 = document.querySelector('.min1');
+let min2 = document.querySelector('.min2');
+
+msec = msec + ((min1 * 10) + min2) * 60;
+
+var sec1 = document.getElementsByClassName('sec1');
+let sec2 = document.querySelector('.sec2');
+
+// let is1 = document.getElementById('is1').textContent = "9";
+// let is2 = document.getElementById('is2').event.target.value;
+// console.log(is1);
+
+msec = msec + ((sec1 * 10) + sec2);
+
+reset = () => {
+    let r = document.querySelector('.resetm').style.display = "block";
+}
+
 btn = () => {
-    setTimeout(fun = () =>{
+    setTimeout(fun = () => {
         console.log('hello');
         // console.log(sec1.innerHTML);
         sec2.innnerHTML = '7';
-    }, 1000 );
+    }, 1000);
 }
